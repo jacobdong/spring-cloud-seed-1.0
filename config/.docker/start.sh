424e0813afd9f83bc979a06fa2345a8a5b798fb9
jacobdong <jacobdong@qq.com>
2016-06-25 14:17:19 +0800

    添加config模块docker构建

---
+++ config/.docker/start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run -d --name @pom.artifactId@_@pom.version@ -p 8888:8888
--- global_build.sh
+++ global_build.sh
@@ -9,16 +9,26 @@ mvn clean install
 echo "## 项目构建完成"
 
 #打包 发现服务
-chmod 777 ./discovery/target/build.sh
+chmod 777 ./**/target/build.sh
+
 echo "## 修改脚本执行权限"
 
 cd ./discovery/target/
 
-echo "## 进入脚本目录"; pwd
+echo "## 进入discovery脚本目录"; pwd
 
 echo "## 准备执行 discovery构建脚本";
 ./build.sh
 
-echo "## 发现服务构建docker镜像完成"
+echo "## discovery 构建docker镜像完成"
+
+cd ../../config/
+
+echo "## 进入config脚本目录"; pwd
+
+echo "## 准备执行 config构建脚本";
+./build.sh
+
+echo "## config 构建docker镜像完成"
 
 docker images
